12ce75bf26f89d20d36bdf906d562e00090f4349
gonzalo <gdurang07@suarezdefigueroa.es>
2025-12-28 21:17:36 +0100

    Convirtiendo cursor de tanques a lista

--- backend/database.py
+++ backend/database.py
@@ -41,7 +41,7 @@ def get_tanks_collection():
     """
     Devuelve la colección 'tanks' de la base de datos.
     """
-    return database["tanks"].find().sort([("rating", -1), ("nacion", 1)])
+    return database["tanks"].find().sort([("rating", -1), ("nacion", 1)]).to_list()
 
 def get_users_collection():
     """
